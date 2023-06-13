f8fa42f6f9f0adce02ad8d9922c09b673bb44699
Gihozo23 <109101706+Gihozo23@users.noreply.github.com>
2023-06-13 14:55:13 +0300

    Create DeleteOccurrencesOfAnElementIfItOccursMoreThanNTimes.test.js

---
+++ Delete occurrences of an element if it occurs more than n times/DeleteOccurrencesOfAnElementIfItOccursMoreThanNTimes.test.js
@@ -0,0 +1,8 @@
+const { assert } = require('chai');
+
+describe("Tests", () => {
+  it("test", () => {
+    assert.sameOrderedMembers(deleteNth([20,37,20,21], 1), [20,37,21])
+    assert.sameOrderedMembers(deleteNth([1,1,3,3,7,2,2,2,2], 3), [1, 1, 3, 3, 7, 2, 2, 2])
+  });
+});
