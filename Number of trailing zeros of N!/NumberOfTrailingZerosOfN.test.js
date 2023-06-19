2d401a4d875c576f68261b66fc4457a7393438d9
Gihozo23 <109101706+Gihozo23@users.noreply.github.com>
2023-06-19 20:11:08 +0300

    Create NumberOfTrailingZerosOfN.test.js

---
+++ Number of trailing zeros of N!/NumberOfTrailingZerosOfN.test.js
@@ -0,0 +1,10 @@
+const Test = require('@codewars/test-compat');
+
+describe("Sample Tests", function() {
+  it("Should pass sample tests", function() {
+    Test.assertEquals(zeros(0), 0, "Testing with n = 0")
+    Test.assertEquals(zeros(5), 1, "Testing with n = 5")
+    Test.assertEquals(zeros(6), 1, "Testing with n = 6")
+    Test.assertEquals(zeros(30), 7, "Testing with n = 30")
+  });
+});
