ffa3a0281a0c372af47f7d7c9c7bc060df3dc40a
Gihozo23 <christellegihozo23@gmail.com>
2024-07-16 16:43:31 +0200

    chore: added some changes to the generateShape function

--- TypeScript/generateShape.ts
+++ TypeScript/generateShape.ts
@@ -1,9 +1,11 @@
 function generateShape(int: number): string {
-    // let result: string = "";
-    // for (let i:number = 0; i < int; i++){
-    //     result += "+".repeat(int);
-    //     result += `\n`
-    // }
-    // return result.trim(); // your code here
+    let result: string = "";
+    for (let i:number = 0; i < int; i++){
+        result += "+".repeat(int);
+        result += `\n`
+    }
+    return result.trim(); // your code here
 }
 generateShape(8)
+
+
