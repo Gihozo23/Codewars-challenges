744dab50a7eb06ce4139848f3b3e75fb8c05809a
Gihozo23 <christellegihozo23@gmail.com>
2024-08-06 12:26:29 +0200

    chore: add multiplication table

---
+++ TypeScript/multiplicationTable.ts
@@ -0,0 +1,14 @@
+export function multiplicationTable (size: number): number[][] {
+    const level1: number[][] = [];
+    const array = [];
+    for (let i = 1; i < size; i++){
+        array.push(i);
+        for (let j = size; i > 0; i++){
+            
+        }
+    }
+    level1.push(array);
+    console.log(level1);
+    return level1;
+}
+  multiplicationTable(4);
