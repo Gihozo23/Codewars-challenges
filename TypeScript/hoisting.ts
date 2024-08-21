4eca3f8a6ba7791d0eef38366fe4973f35bcb87f
Gihozo23 <christellegihozo23@gmail.com>
2024-08-21 19:32:58 +0200

    chore: add definition to hoisting.js

---
+++ TypeScript/hoisting.ts
@@ -0,0 +1,10 @@
+//How are variables declared with var are hoisted ?
+
+//Hoisting is a mechanism in JavaScript where declarations of variables are moved to the top of their scope
+// before the codes are executed.
+
+//global scope:
+
+
+
+
