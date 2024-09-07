e839eeabc835c81a5f1ff344e63d2cf284953095
Gihozo23 <christellegihozo23@gmail.com>
2024-09-07 18:55:50 +0200

    refactor: add test to see how singleton class works

--- TypeScript/singletonsClass.ts
+++ TypeScript/singletonsClass.ts
@@ -1,18 +1,21 @@
 export class SingletonCounter {
-    public count;
-    private static myInstance: SingletonCounter;
-    private constructor(){
-        this.count = 0;
-      }
-    static getInstance(){
-      if(!SingletonCounter.myInstance){
-       SingletonCounter.myInstance = new SingletonCounter();
-        }
-        return SingletonCounter.myInstance;
-        
-      }
-    inc() {
-      this.count++;
-      return this.count;
-      }
+  public count;
+  private static myInstance: SingletonCounter;
+  private constructor() {
+    this.count = 0;
+  }
+  static getInstance() {
+    if (!SingletonCounter.myInstance) {
+      SingletonCounter.myInstance = new SingletonCounter();
     }
+    return SingletonCounter.myInstance;
+  }
+  inc() {
+    this.count++;
+    return this.count;
+  }
+}
+const inst = SingletonCounter.getInstance();
+console.log(inst.inc());
+const inst22 = SingletonCounter.getInstance();
+console.log(inst22.inc())
